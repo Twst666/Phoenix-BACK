669c8b5650fb66b26e45d70b0091ad1ab580f2df
Twst666 <141984610+Twst666@users.noreply.github.com>
2024-10-20 14:46:59 +0530

    Update group.js

--- plugins/group.js
+++ plugins/group.js
@@ -11,7 +11,9 @@ pnix({
     let isadmin = await isAdmin(message.jid, message.user, message.client);
     if (!isadmin) return await message.reply("_* I M Not An Admin!*_");
     const data = await message.client.groupInviteCode(message.jid);
-    return await message.reply(`https://chat.whatsapp.com/${data}`);
+    const metadata = await message.client.groupMetadata(message.jid);
+    const { subject } = metadata;
+    return await message.reply(`╭───❮ *ɢʀᴏᴜᴘ ʟɪɴᴋ* ❯\n*ɢʀᴏᴜᴘ:* ${subject}\n*ʟɪɴᴋ:* https://chat.whatsapp.com/${data}`);
 });
 
 pnix({
@@ -143,44 +145,25 @@ pnix(
   }
 );
 
-pnix({
-    pattern: "tag",
-    fromMe: true,
-    desc: "mention all users in the group",
+pnix(
+  {
+    pattern: "tagall",
+    fromMe: isPrivate,
     type: "group",
-}, async (message, match, m) => {
-    if (!message.isGroup) 
+  },
+  async (message, match) => {
+    if (!message.isGroup)
     return await message.reply("_*This Command Is Only For Groups!*_");
-    const { participants } = await message.client.groupMetadata(message.from).catch(e => {});
-    let admins = await participants.filter(v => v.admin !== null).map(v => v.id);
-    let msg = "";
-    if (match === "all" || match === "everyone") {
-        for (let i = 0; i < participants.length; i++) {
-            msg += `${i + 1}. @${participants[i].id.split('@')[0]}\n`;
-        }
-        await message.send(msg, { mentions: participants.map(a => a.id) });
-    } 
-    else if (match === "admin" || match === "admins") {
-        for (let i = 0; i < admins.length; i++) {
-            msg += `${i + 1}. @${admins[i].split('@')[0]}\n`;
-        }
-        return await message.send(msg, { mentions: participants.map(a => a.id) });
-    } 
-    else if (match === "me" || match === "mee") {
-        await message.client.sendMessage(message.chat, { text: `@${message.sender.split("@")[0]}`, mentions: [message.sender] });
-    } 
-    else if (match || message.reply_message.text) {
-        match = match || message.reply_message.text;
-        if (!match) return await message.reply(`_Reply To A Message_\n_📌 Example:\n_*${m.prefix}tag all*_\n_*${m.prefix}tag admin*_\n_*${m.prefix}tag text*_`);
-        await message.send(match, { mentions: participants.map(a => a.id) });
-    } 
-    else if (message.reply_message.i) {
-        return await message.client.forwardMessage(message.jid, message.reply_message.message, { contextInfo: { mentionedJid: participants.map(a => a.id) } });
-    } 
-    else {
-        return await message.reply(`_Reply To A Message_\n_📌 Example:\n_*${m.prefix}tag all*_\n*_${m.prefix}tag admin*_\n*_${m.prefix}tag text*_`);
+    const { participants } = await message.client.groupMetadata(message.jid);
+    let teks = "";
+    for (let mem of participants) {
+      teks += ` @${mem.id.split("@")[0]}\n`;
     }
-});
+    message.sendMessage(teks.trim(), {
+      mentions: participants.map((a) => a.id),
+    });
+  }
+);
 
 pnix(
   {
